a6bb9348452b395344422cf49c783355a1625ea4
edsonBJR <ebjr.tux@gmail.com>
2020-09-13 05:38:48 -0300

    Projeto Criado

---
+++ src/main/java/com/sgcpf/SgcpfApplication.java
@@ -0,0 +1,13 @@
+package com.sgcpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcpfApplication.class, args);
+	}
+
+}
